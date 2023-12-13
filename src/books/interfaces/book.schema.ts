bc23fdea4290d9aa2b764b1e518d23b3d72f08f7
Nataliya-grish <gray-wings@yandex.ru>
2023-12-13 19:51:48 +0300

    done

---
+++ src/books/interfaces/book.schema.ts
@@ -0,0 +1,27 @@
+export type BookDocument = Book & Document;
+
+@Schema()
+export class Book extends Document {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop()
+  description: string;
+
+  @Prop()
+  authors: string;
+
+  @Prop()
+  favorite: string;
+
+  @Prop()
+  fileCover: string;
+
+  @Prop()
+  fileName: string;
+
+  @Prop()
+  fileBook: string;
+}
+
+export const BookSchema = SchemaFactory.createForClass(Book);
